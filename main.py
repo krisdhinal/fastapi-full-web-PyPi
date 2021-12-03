a4fc0072ef7602c05e8938f8354b27a4ad910788
krisdhinal <krisdhinal@gmail.com>
2021-12-03 15:37:55 +0700

    initial project setup

---
+++ main.py
@@ -0,0 +1,20 @@
+import fastapi
+import uvicorn
+import fastapi_chameleon
+from fastapi_chameleon import template
+
+app = fastapi.FastAPI()
+
+fastapi_chameleon.global_init('templates')
+
+
+@app.get('/')
+@template(template_file='index.html')
+def index(user: str = 'anonimus'):
+    return {
+        'user_name': user
+    }
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
